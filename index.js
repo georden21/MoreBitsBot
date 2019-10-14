92aca2815e7fef1709f08f8a9aef97d4b2a19391
Kokotos Sakis(sakis coc) <georden21@hotmail.gr>
2019-10-15 01:30:19 +0300

    new commit

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
 
-const dotenv = require('dotenv');
-const path = require('path');
-const restify = require('restify');
+var useEmulator = true;
+var userName = "";
+var yearsCoding = "";
+var selectedLanguage = "";
 
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter } = require('botbuilder');
-
-// This bot's main dialog.
-const { EchoBot } = require('./bot');
-
-// Import required bot configuration.
-const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
-
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
-});
-
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
-const adapter = new BotFrameworkAdapter({
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    channelService: process.env.ChannelService,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    appPassword: process.env.MicrosoftAppPassword
 });
 
-// Catch-all for errors.
-adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    console.error(`\n [onTurnError]: ${ error }`);
-    // Send a message to the user
-    await context.sendActivity(`Oops. Something went wrong!`);
-};
+var bot = new builder.UniversalBot(connector);
 
-// Create the main dialog.
-const bot = new EchoBot();
+bot.dialog('/', [
 
-// Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await bot.run(context);
-    });
+    function (session) {
+        builder.Prompts.text(session, "Hello, and welcome to QnA Factbot! What's your name?");
+    },
+
+    function (session, results) {
+        userName = results.response;
+        builder.Prompts.number(session, "Hi " + userName + ", how many years have you been writing code?");
+    },
+
+    function (session, results) {
+        yearsCoding = results.response;
+        builder.Prompts.choice(session, "What language do you love the most?", ["C#", "Python", "Node.js", "Visual FoxPro"]);
+    },
+
+    function (session, results) {
+        selectedLanguage = results.response.entity;
+
+        session.send("Okay, " + userName + ", I think I've got it:" +
+            " You've been writing code for " + yearsCoding + " years," +
+            " and prefer to use " + selectedLanguage + ".");
+    }]);
+
+var restify = require('restify');
+var server = restify.createServer();
+
+server.listen(3978, function () {
+    console.log('test bot endpoint at http://localhost:3978/api/messages');
 });
+
+server.post('/api/messages', connector.listen());
